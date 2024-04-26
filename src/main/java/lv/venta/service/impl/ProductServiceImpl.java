9b8efee2b5d748fdfd8a79218b2cbd0071849f78
Kelechi100 <kintucis6@gmail.com>
2024-04-26 16:09:27 +0300

    Added CRUD testing in Service

--- src/main/java/lv/venta/service/impl/ProductServiceImpl.java
+++ src/main/java/lv/venta/service/impl/ProductServiceImpl.java
@@ -2,9 +2,11 @@ package lv.venta.service.impl;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 import lv.venta.service.IProductFilteringService;
 import lv.venta.service.ProductCRUDService;
 
@@ -12,6 +14,9 @@ import lv.venta.service.ProductCRUDService;
 
 public class ProductServiceImpl implements ProductCRUDService, IProductFilteringService{
 
+	@Autowired
+	private IProductRepo productRepo;
+	
 	@Override
 	public ArrayList<Product> filterByQuantityThreshold(int threshold) {
 		// TODO Auto-generated method stub
@@ -32,26 +37,45 @@ public class ProductServiceImpl implements ProductCRUDService, IProductFiltering
 
 	@Override
 	public ArrayList<Product> retrieveAll() throws Exception {
+		if(productRepo.count() == 0) throw new Exception("Product table is empty");
+		ArrayList<Product> result = (ArrayList<Product>) productRepo.findAll();
 		// TODO Auto-generated method stub
-		return null;
+		return result;
 	}
 
 	@Override
 	public Product retrievById(int id) throws Exception {
+		if(id <= 0) throw new Exception("Id should be positive");
+		
+		if(productRepo.existsById(id)) return productRepo.findById(id).get();
 		// TODO Auto-generated method stub
-		return null;
+		
+		throw new Exception("Product with id ( " + id +  ") is not in the Product table");
 	}
 
 	@Override
 	public void updateById(int id, String title, String description, float price, int quantity) throws Exception {
+		if(title == null|| description == null || price < 0 || quantity < 0 )
+			throw new Exception("Problem with input paramaters");
+		Product productForUpdating = retrievById(id);
+		productForUpdating.setTitle(title);
+		productForUpdating.setDescription(description);
+		productForUpdating.setPrice(price);
+		productForUpdating.setQuantity(quantity); 
+		
+		productRepo.save(productForUpdating);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void deleteById(int id) throws Exception {
+		Product ProductForDeleting = retrievById(id);
+		productRepo.delete(ProductForDeleting);
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 
 }
