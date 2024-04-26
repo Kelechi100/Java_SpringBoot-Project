a50ad1c3c02ebcf0a69233ac56517f6d3ef949f1
Kelechi100 <kintucis6@gmail.com>
2024-04-26 14:24:08 +0300

    Update: added database to model Class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +17,25 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	@Column(name  = "TITLE")
 	private String title;
+	
+	@Column(name = "DESCRIPTION")
 	private String description;
+	
+	@Column(name = "PRICE")
 	private float price;
+	
+	@Column(name = "QUANTITY")
 	private int quantity;
 	
 	private static int counter = 1;	
@@ -27,7 +46,7 @@ public class Product {
 	
 	
 	public Product(String title, String description, float price, int quantity) {
-		setId();
+	
 		setTitle(title);
 		setDescription(description);
 		setPrice(price);
